26db83ffe2103461125c98ed3ee4e9fb42b0e3c5
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-16 19:40:07 +0200

    imported essentials models

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MissionController;
+use App\Http\Middleware\ValidateToken;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
