d99be353e4f402dc38b14132e332fdfbe2fc72d3
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-16 18:47:38 +0200

    init commit

---
+++ database/migrations/2025_08_16_162940_create_astronauts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('astronauts', function (Blueprint $table) {
+            $table->uuid("id") -> primary();
+            $table->string("name", 50);
+            $table->integer("endurance");
+            $table->integer("skill");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('astronauts');
+    }
+};
---
+++ database/migrations/2025_08_16_162952_create_missions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('missions', function (Blueprint $table) {
+            $table->uuid("id") -> primary();
+            $table->foreignUuid("destination_id") -> constrained();
+            $table->string("captain", 255);
+            $table->string("report", 2048);
+            $table->boolean("success");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('missions');
+    }
+};
